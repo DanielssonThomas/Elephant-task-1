aad831583f0b69d40e17b939ac6b3f35e2ec13ae
Thomas Danielsson <69715206+DanielssonThomas@users.noreply.github.com>
2022-10-19 14:57:14 +0200

    added fonts and main game section
    Montserrat folder was added
    CSS alignment for components in:
    header contents
    main game contents

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/rollStyle.css">
+    <link rel="stylesheet" href="./CSS/typography.css">
+    <link rel="stylesheet" href="./CSS/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    require('header.php');
+    require('main-roll.php');
+    require('footer.php');
+    ?>
+</body>
+
+</html>
---
+++ main-roll.php
@@ -0,0 +1,13 @@
+<?php
+$rollValue = 1000000;
+
+?>
+
+<main class="roll-main">
+    <section class="roll-game-container">
+        <h2>
+            <?= $rollValue ?>
+        </h2>
+
+    </section>
+</main>
